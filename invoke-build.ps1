a09e6c54eccb884979e6261d5efa6af79589ddf3
Paul H Cassidy <qawarrior@gmail.com>
2017-04-06 10:46:21 -0700

    Updating Sources

--- invoke-build.ps1
+++ invoke-build.ps1
@@ -3,19 +3,22 @@
 Param (
 )
 
+$hugoDir = '/github/QawarriorSite'
 $publishDir = '/github/qawarrior.github.io'
-$excludePatterns = @(
-    '.git/*.*')
-$expressions = @(
-    "hugo --destination $publishDir", 
-    "git add --all", 'git commit -m "Updated Site"', 
-    'git push origin master')
+$excludePatterns = '.git/*.*'
 
-$( Get-ChildItem -Path $publishDir -Recurse -Exclude $excludePattern ).FullName |
-Remove-Item -Recurse -Force -ErrorAction 'SilentlyContinue'
-
-$expressions |
-ForEach-Object {
-    Start-Sleep -Seconds 1  
-    Invoke-Expression -Command $PSItem -Verbose
+function Invoke-GitCmd () {
+    Invoke-Expression -Command 'git checkout master'
+    Invoke-Expression -Command 'git add --all'
+    Invoke-Expression -Command 'git commit -m "Updating Sources"'
 }
+
+Push-Location -Path $publishDir
+Invoke-Expression -Command 'git checkout master'
+Start-Sleep -Seconds 1
+Pop-Location
+
+Push-Location -Path $hugoDir
+Invoke-Expression -Command "hugo --destination $publishDir"
+Pop-Location
+
---
+++ temp-buteumfuex.ps1
@@ -0,0 +1,3 @@
+Invoke-Expression -Command 'git checkout master'
+    Invoke-Expression -Command 'git add --all'
+    Invoke-Expression -Command 'git commit -m "Updating Sources"'
